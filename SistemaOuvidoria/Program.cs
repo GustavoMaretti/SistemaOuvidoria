c76a6abcf26fe61e5ec54a7a60d3c0923e80b9db
Gustavo Maretti <gustavohmaretti@outlook.com>
2026-09-03 08:31:40 -0300

    Criação da estrutura de pastas do projeto

---
+++ SistemaOuvidoria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SistemaOuvidoria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
